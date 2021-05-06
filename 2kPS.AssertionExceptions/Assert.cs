07ebe3450b62fc7e86e9c810b348d7ef88ba98a4
Carlos DaLomba <carlos@2kpixelstudios.net>
2021-05-06 01:53:58 -0400

    Added 1st iteration of basic Assert.IsTrue(...)

---
+++ 2kPS.AssertionExceptions/Assert.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace PS2k.AssertionExceptions {
+    /// <summary>
+    /// Contains assertions that may be used outside of unit tests, and are only included in Debug builds.
+    /// </summary>
+    public static class Assert {
+        [Conditional("DEBUG")]
+        public static void IsTrue(bool condition) {
+            if (!condition)
+                throw new AssertionException();
+        }
+
+        [Conditional("DEBUG")]
+        public static void IsTrue(bool condition, string message) {
+            if (!condition)
+                throw new AssertionException(message);
+        }
+    }
+}
---
+++ 2kPS.AssertionExceptions/AssertionException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PS2k.AssertionExceptions {
+    /// <summary>
+    /// <para>An <see cref="Exception"/> that is thrown when an assertion is failed.</para>
+    /// <para>This should signify that critical logic within your code was broken, and code changes will be required to fix the program logic to work as intended.</para>
+    /// </summary>
+    public class AssertionException : Exception {
+        public AssertionException() { }
+        public AssertionException(string message) : base(message) { }
+    }
+}
